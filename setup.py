9f6226f84dadc4532a5d94971c9ab46a70e13241
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2017-10-26 16:38:58 +0330

    Add Setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'Arthon',
+  packages = ['Arthon'],
+  version = '1.0',
+  description = 'Art + Python\nASCII Art Maker',
+  author = 'MohamadKh75',
+  author_email = 'khajavi_mohamad@yahoo.com',
+  url = 'https://github.com/MohamadKh75/Arthon',
+  download_url = 'https://github.com/MohamadKh75/Arthon',
+  keywords = ['ASCII', 'art', 'python3', 'text'],
+  classifiers = [],
+  license='MIT',
+)
